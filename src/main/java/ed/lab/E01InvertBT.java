06bd0fe1f7efd9a1a06b41253177760271e3101b
AndryuGC <andryupgc@gmail.com>
2025-03-24 20:20:02 -0600

    Prueba 03

--- src/main/java/ed/lab/E01InvertBT.java
+++ src/main/java/ed/lab/E01InvertBT.java
@@ -2,6 +2,19 @@ package ed.lab;
 
 public class E01InvertBT {
     public TreeNode<Integer> invertTree(TreeNode<Integer> root) {
+        if (root == null) {
+            return null;
+        }
+
+        // Intercambiar los nodos hijos
+        TreeNode<Integer> temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        // Llamar recursivamente en los subárboles izquierdo y derecho
+        invertTree(root.left);
+        invertTree(root.right);
+
         return root;
     }
 }
